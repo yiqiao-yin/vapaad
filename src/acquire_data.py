3b5bd440ee805f7408dccc2746e17a210125daaf
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2024-05-07 15:43:47 -0400

    Create acquire_data.py

---
+++ src/acquire_data.py
@@ -0,0 +1,46 @@
+import numpy as np
+import keras
+from keras import layers
+
+# Download and load the dataset.
+fpath = keras.utils.get_file(
+    "moving_mnist.npy",
+    "http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy",
+)
+dataset = np.load(fpath)
+
+# Swap the axes representing the number of frames and number of data samples.
+dataset = np.swapaxes(dataset, 0, 1)
+# We'll pick out 1000 of the 10000 total examples and use those.
+# dataset = dataset[:1000, ...]
+# Add a channel dimension since the images are grayscale.
+dataset = np.expand_dims(dataset, axis=-1)
+
+# Split into train and validation sets using indexing to optimize memory.
+indexes = np.arange(dataset.shape[0])
+np.random.shuffle(indexes)
+train_index = indexes[: int(0.9 * dataset.shape[0])]
+val_index = indexes[int(0.9 * dataset.shape[0]) :]
+train_dataset = dataset[train_index]
+val_dataset = dataset[val_index]
+
+# Normalize the data to the 0-1 range.
+train_dataset = train_dataset / 255
+val_dataset = val_dataset / 255
+
+
+# We'll define a helper function to shift the frames, where
+# `x` is frames 0 to n - 1, and `y` is frames 1 to n.
+def create_shifted_frames(data):
+    x = data[:, 0 : data.shape[1] - 1, :, :]
+    y = data[:, 1 : data.shape[1], :, :]
+    return x, y
+
+
+# Apply the processing function to the datasets.
+x_train, y_train = create_shifted_frames(train_dataset)
+x_val, y_val = create_shifted_frames(val_dataset)
+
+# Inspect the dataset.
+print("Training Dataset Shapes: " + str(x_train.shape) + ", " + str(y_train.shape))
+print("Validation Dataset Shapes: " + str(x_val.shape) + ", " + str(y_val.shape))
